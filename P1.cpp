c6ab506e01b9bdafdcd20a51766586e0a5d96c07
Josh <dash219@gmail.com>
2018-09-26 13:41:42 -0700

    i made P1.cpp twenty times better
    it's vastly improved

--- P1.cpp
+++ P1.cpp
@@ -7,6 +7,8 @@ int main ()
 	//LOOP
 		//start the clock
 		//read all the bytes like, 10 times?
+			//read them mod a prime number so that we can foil AMD/Intel
+				//hahahahaha
 		//stop the clock
 		//shuffle all of the bytes around, somehow
 		//compute N/time elapsed
