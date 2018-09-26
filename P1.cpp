096af213b21b6ae7607309dcee4e8ea6a772b527
Josh <dash219@gmail.com>
2018-09-26 13:38:28 -0700

    added P1.cpp
    this is an outline of problem 1

---
+++ P1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	//generate an array of 2^(N) random bytes
+	//LOOP
+		//start the clock
+		//read all the bytes like, 10 times?
+		//stop the clock
+		//shuffle all of the bytes around, somehow
+		//compute N/time elapsed
+		//print it!
+		//back to the top!
+	return
+}
