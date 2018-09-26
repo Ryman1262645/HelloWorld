912ad73070595177b0f22877bc6ea1c3a561a012
Josh <dash219@gmail.com>
2018-09-26 13:56:21 -0700

    more work on P1.cpp
    did first set

--- P1.cpp
+++ P1.cpp
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-int main ()
+int8_t* generate_random_list(int64_t size, int16_t bound)
 {
-	//generate an array of 2^(N) random bytes
+	uint8_t* list;
+	list = malloc(sizeof(uint8_t) * size);
+	if(!list){/*death*/}
+
+	srand(size);
+	for(int64_t i = 0; i < size; i++)
+	{
+		list[i] = (rand() % bound);
+	}
+	return list;
+}
+
+int main (int argc, char **argv)
+{
+	//take arguments
+	int64_t size = atoi(argv[1]);
+	int64_t iters = atoi(argv[2]);
+
+	//generate an array of 2^(N) random byte
+	uint8_t *arrboy = generate_random_list(size, 255);
+
 	//LOOP
 		//start the clock
-		//read all the bytes like, 10 times?
+		//read all the bytes (iters) times
 			//read them mod a prime number so that we can foil AMD/Intel
 				//hahahahaha
 		//stop the clock
